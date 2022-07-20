09700f776a9bed65a97572ed715b712ae97d244c
Alphacentaura <alphacentaura@bk.ru>
2022-07-20 16:23:37 +0300

    Rewrited methods to work with our DB

--- book-list/main.go
+++ book-list/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/lib/pq"
 	"github.com/subosito/gotenv"
@@ -38,6 +39,8 @@ func main() {
 	db, err = sql.Open("postgres", pgUrl)
 	logFatal(err)
 
+	//log.Println(pgUrl)
+
 	err = db.Ping()
 	logFatal(err)
 
@@ -53,16 +56,72 @@ func main() {
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	books = []Book{}
+
+	rows, err := db.Query("select * from books")
+	logFatal(err)
+
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		logFatal(err)
+	}(rows)
+
+	for rows.Next() {
+		err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+		logFatal(err)
+
+		books = append(books, book)
+	}
+
+	err = json.NewEncoder(w).Encode(books)
+	logFatal(err)
 }
 
 func getBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	params := mux.Vars(r)
+
+	rows := db.QueryRow("select * from books where id=$1", params["id"])
+
+	err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(book)
 }
 
 func addBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	var bookId int
+
+	json.NewDecoder(r.Body).Decode(&book)
+
+	err := db.QueryRow("insert into books (title, author, year) values($1, $2, $3) RETURNING id", book.Title, book.Author, book.Year).Scan(&bookId)
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(bookId)
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	json.NewDecoder(r.Body).Decode(&book)
+
+	result, err := db.Exec("update books set title=$1, author = $2, year = $3 where id = $4 RETURNING id", &book.Title, &book.Author, &book.Year, &book.ID)
+
+	rowsUpdated, err := result.RowsAffected()
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(rowsUpdated)
 }
 
 func removeBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	result, err := db.Exec("delete from books where id = $1", params["id"])
+	logFatal(err)
+
+	rowsDeleted, err := result.RowsAffected()
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(rowsDeleted)
 }
